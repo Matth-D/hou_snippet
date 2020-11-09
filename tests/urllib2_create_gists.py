f29db6bd5d7703d891fcdab7a93ac6ebf43c5e88
Matth-D <matthieu.druaud@gmail.com>
2020-11-09 13:57:15 +0000

    tests

---
+++ tests/urllib2_create_gists.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import json
+import urllib
+import urllib2
+import base64
+
+# auth_file =
+
+# with open(auth_file, "r") as auth_file:
+#     auth_data = json.load(auth_file)
+
+# username = auth_data["username"]
+# full_token = auth_data["full_token"]
+# gh_api_url = "https://api.github.com"
+# gh_url = "https://github.com"
+# usr_gh_url = gh_api_url + "/users/{0}".format(username)
+
+# gist_name = "created from vscode"
+# gist_content = "gists body sent from vscode"
+
+# gist_args = utils.format_gist(gist_name, True, "test_gists.py", gist_content)
+# print gist_args
