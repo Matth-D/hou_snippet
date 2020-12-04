5cf1b8adcc46d91358a0ef2d82c89a858edd757c
Matthieu Druaud <matthieu.druaud@gmail.com>
2020-12-04 17:33:31 +0000

    /

--- src/h_snippet/test_node_name_recursion.py
+++ src/h_snippet/test_node_name_recursion.py
@@ -1,1 +1,10 @@
 node_list = ["node", "node_1", "node_2"]
+
+
+node_name = "node"
+if node_name in node_list:  # check if node already exists, use path here
+    i = 1
+    new_name = node_name + "_" + str(i)
+    while new_name in node_list:
+        i += 1
+        print(new_name)
