c3e8ff5340d8b6438627a08a18d3561a15370879
Matth-D <matthieu.druaud@gmail.com>
2021-01-04 23:17:07 +0000

    cron test 4

--- src/h_snippet/clean_gist_repo.py
+++ src/h_snippet/clean_gist_repo.py
@@ -20,27 +20,30 @@ with open(auth_file_path, "r") as auth_file:
     AUTH_DATA = json.load(auth_file)
 CERTIF_FILE = certifi.where()
 
+print AUTH_DATA["username"]
+print AUTH_DATA["gist_token"]
+print CERTIF_FILE
 
-def delete_all_gists():
-    gists_url = "https://api.github.com/users/houdini-snippet/gists"
-    gists_response = urllib2.urlopen(gists_url, cafile=CERTIF_FILE)
-    gists = json.loads(gists_response.read())
-    gists_list = []
+# def delete_all_gists():
+#     gists_url = "https://api.github.com/users/houdini-snippet/gists"
+#     gists_response = urllib2.urlopen(gists_url, cafile=CERTIF_FILE)
+#     gists = json.loads(gists_response.read())
+#     gists_list = []
 
-    if not gists:
-        return
-    for gist in gists:
-        gists_list.append(gist["url"])
+#     if not gists:
+#         return
+#     for gist in gists:
+#         gists_list.append(gist["url"])
 
-    b64str = base64.b64encode(
-        "{0}:{1}".format(AUTH_DATA["username"], AUTH_DATA["gist_token"])
-    )
-    request_method = "DELETE"
-    for url in gists_list:
-        request = urllib2.Request(url)
-        request.add_header("Authorization", "Basic {0}".format(b64str))
-        request.get_method = lambda: request_method
-        response = urllib2.urlopen(request, cafile=CERTIF_FILE)
+#     b64str = base64.b64encode(
+#         "{0}:{1}".format(AUTH_DATA["username"], AUTH_DATA["gist_token"])
+#     )
+#     request_method = "DELETE"
+#     for url in gists_list:
+#         request = urllib2.Request(url)
+#         request.add_header("Authorization", "Basic {0}".format(b64str))
+#         request.get_method = lambda: request_method
+#         response = urllib2.urlopen(request, cafile=CERTIF_FILE)
 
 
-delete_all_gists()
+# delete_all_gists()
