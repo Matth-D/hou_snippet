9c1781c431a99493f55643008783a9ddcbe08171
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-01-05 17:56:01 +0000

    change cleangist function

--- src/h_snippet/clean_gist_repo.py
+++ src/h_snippet/clean_gist_repo.py
@@ -25,27 +25,37 @@ CERTIF_FILE = certifi.where()
 # figure out error 401 in github actions
 
 
-def delete_all_gists():
-    gists_url = "https://api.github.com/users/houdini-snippet/gists"
-    gists_response = urllib2.urlopen(gists_url, cafile=CERTIF_FILE)
-    gists = json.loads(gists_response.read())
-    gists_list = []
+# def delete_all_gists():
+#     gists_url = "https://api.github.com/users/houdini-snippet/gists"
+#     gists_response = urllib2.urlopen(gists_url, cafile=CERTIF_FILE)
+#     gists = json.loads(gists_response.read())
+#     gists_list = []
 
-    if not gists:
-        return
-    for gist in gists:
-        gists_list.append(gist["url"])
+#     if not gists:
+#         return
+#     for gist in gists:
+#         gists_list.append(gist["url"])
 
-    b64str = base64.b64encode(
-        "{0}:{1}".format(AUTH_DATA["username"], AUTH_DATA["gist_token"])
-    )
-    request_method = "DELETE"
-    for url in gists_list:
-        request = urllib2.Request(url)
-        request.add_header("Authorization", "Basic {0}".format(b64str))
-        request.add_header("User-Agent", "Magic Browser")
-        request.get_method = lambda: request_method
-        response = urllib2.urlopen(request, cafile=CERTIF_FILE)
+#     b64str = base64.b64encode(
+#         "{0}:{1}".format(AUTH_DATA["username"], AUTH_DATA["gist_token"])
+#     )
+#     request_method = "DELETE"
+#     for url in gists_list:
+#         request = urllib2.Request(url)
+#         request.add_header("Authorization", "Basic {0}".format(b64str))
+#         request.add_header("User-Agent", "Magic Browser")
+#         request.get_method = lambda: request_method
+#         response = urllib2.urlopen(request, cafile=CERTIF_FILE)
 
 
-delete_all_gists()
+# delete_all_gists()
+
+
+def open_google():
+
+    url = "https://google.com"
+    response = urllib2.urlopen(url)
+    print response.read()
+
+
+open_google()
