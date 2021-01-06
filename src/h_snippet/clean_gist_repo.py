9166b52be9a93bd6ed1ccdbbbbcdbe35ac7179be
Matthieu Druaud <matthieu.druaud@gmail.com>
2021-01-06 12:06:58 +0000

    test run on macos

--- src/h_snippet/clean_gist_repo.py
+++ src/h_snippet/clean_gist_repo.py
@@ -22,38 +22,6 @@ with open(auth_file_path, "r") as auth_file:
 CERTIF_FILE = certifi.where()
 
 
-# figure out error 401 in github actions
-
-print AUTH_DATA["gist_token"]
-print AUTH_DATA["username"]
-
-
-# def delete_all_gists():
-#     gists_url = "https://api.github.com/users/houdini-snippet/gists"
-#     gists_response = urllib2.urlopen(gists_url, cafile=CERTIF_FILE)
-#     gists = json.loads(gists_response.read())
-#     gists_list = []
-
-#     if not gists:
-#         return
-#     for gist in gists:
-#         gists_list.append(gist["url"])
-
-#     b64str = base64.b64encode(
-#         "{0}:{1}".format(AUTH_DATA["username"], AUTH_DATA["gist_token"])
-#     )
-#     request_method = "DELETE"
-#     for url in gists_list:
-#         request = urllib2.Request(url)
-#         request.add_header("Authorization", "Basic {0}".format(b64str))
-#         request.add_header("User-Agent", "Magic Browser")
-#         request.get_method = lambda: request_method
-#         response = urllib2.urlopen(request, cafile=CERTIF_FILE)
-
-
-# delete_all_gists()
-
-
 def delete_all_gists():
     gists_url = "https://api.github.com/users/houdini-snippet/gists"
     gists_response = urllib2.urlopen(gists_url, cafile=CERTIF_FILE)
@@ -65,8 +33,16 @@ def delete_all_gists():
     for gist in gists:
         gists_list.append(gist["url"])
 
-    print gists_list
-    return
+    b64str = base64.b64encode(
+        "{0}:{1}".format(AUTH_DATA["username"], AUTH_DATA["gist_token"])
+    )
+    request_method = "DELETE"
+    for url in gists_list:
+        request = urllib2.Request(url)
+        request.add_header("User-Agent", "Magic Browser")
+        request.add_header("Authorization", "Basic {0}".format(b64str))
+        request.get_method = lambda: request_method
+        response = urllib2.urlopen(request, cafile=CERTIF_FILE)
 
 
 delete_all_gists()
